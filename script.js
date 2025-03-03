9714b36b791e567019cbd79c41fa0dce0cfb9c63
vela0474 <vela0474@student.acitech.org>
2025-03-03 09:12:14 -0500

    I did nothing at all, button doesn't work at all.

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const button1 = document.getElementById("button1");
+let text = document.getElementById("text");
+let button1 = document.getElementById("button1");
+
+let clicks = 0;
 
 button1.addEventListener("click", yell_at_user);
 
-function yell_at_user () {
-    alert("STOP PRESSING THIS BUTTON");
+function yell_at_user() {
+   
+    clicks++;
+    if( clicks == 1 ) {
+        text.innerHTML = "Hey! Stop that!";
+    }
+    else if( clicks == 5 ) {
+        text.innerHTML = "Leave me alone!!";
+    }
+    else if( clicks == 10) {
+        text.innerHTML = "I'm gonna get you!";
+    }
+
 }
